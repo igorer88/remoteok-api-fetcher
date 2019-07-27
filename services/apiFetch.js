70288c33956961df35acd9ae207a3ebe49ebb8d2
Igor Escalona <igorer88@gmail.com>
2019-07-27 16:40:04 -0400

    Added the http response status management.

--- services/apiFetch.js
+++ services/apiFetch.js
@@ -1,5 +1,5 @@
 'use strict';
-const Job = require('../db/job');
+
 const axios = require('axios');
 
 axios.defaults.baseURL = 'https://remoteok.io/';
@@ -7,13 +7,25 @@ axios.defaults.baseURL = 'https://remoteok.io/';
 async function get() {
   try {
     const response = await axios.get(`api/`);
-    console.log('Fetching from server...')
+    console.log('Fetching from server...');
     console.log('Response status: ');
     console.log(response.status, response.statusText);
-    console.log('Legal Notice: ', response.data[0].legal);
+
+    if (response.status === 200) {
+        console.log('Legal Notice: ', response.data[0].legal);
+    }
+    if (response.status >= 400 && response.status < 500) {
+        console.error('Maybe the API has changed?');
+    }
+    if (response.status >= 500) {
+        console.error('There seems to be a problem with the server,');
+        console.error('Try again later.');
+    }
     return response;
   } catch (error) {
-    console.error(error);
+    if (error.code === 'ENOTFOUND') {
+      console.error(`Could not connect to the host: ${error.config.url}`);
+    }
   }
 }
 
